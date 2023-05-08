4e0ea98b750c3ad0708aa994fac3c949363897b0
fran fran <franlobogarcia@gmail.com>
2023-05-08 16:55:23 +0200

    fix: added files to repo

--- dist/cjs/types/types/basic.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference types="react" />
-export type Colors = "bwhite" | "bcream" | "bbrown" | "bgray" | "bgreen" | "black" | "bblack" | "bgreyish" | "bblue" | "gray" | "brown" | "transparent";
-export type LayoutWidth = "large" | "narrow" | "medium";
-export interface Container {
-    children: React.ReactNode | React.ReactNode[];
-    className: string;
-}
--- dist/cjs/types/utils/utils.d.ts
+++
@@ -1,2 +0,0 @@
-/// <reference types="node" />
-export declare const timer: (time: number, callback: (params?: any) => any) => NodeJS.Timeout;
